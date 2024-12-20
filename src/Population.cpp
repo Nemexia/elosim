7735335d2148f406e56e66de2906de6f7aa43da1
Nemexia <amirhossein.rahmani55@gmail.com>
2024-12-20 23:30:02 +0330

    Add Population header and source

---
+++ src/Population.cpp
@@ -0,0 +1,18 @@
+/*
+ * Population.cpp
+ *
+ *  Created on: Dec 20, 2024
+ *      Author: Nemexia
+ */
+
+#include "Population.h"
+
+Population::Population() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Population::~Population() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Population.h
@@ -0,0 +1,20 @@
+/*
+ * Population.h
+ *
+ *  Created on: Dec 20, 2024
+ *      Author: Nemexia
+ */
+
+#ifndef POPULATION_H_
+#define POPULATION_H_
+
+class Population {
+public:
+	Population();
+	virtual ~Population();
+private:
+	int size;
+
+};
+
+#endif /* POPULATION_H_ */
